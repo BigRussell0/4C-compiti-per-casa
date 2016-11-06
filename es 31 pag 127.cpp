667ca3607a143472096028acca5e23558d5e6222
BigRussell0 <zetak24@gmail.com>
2016-11-06 22:59:12 +0100

    esercizi per il 5 novembre
    fatto

--- es 31 pag 127.cpp
+++ es 31 pag 127.cpp
@@ -16,3 +16,62 @@ int main ()
   cout<<endl;    
   stampa (valore) ;
   return 0;}
+
+
+esercizio 34 p 127
+  #include <iostream>
+  using namespace std;
+int a;
+int successione ( int a)
+{
+  int b;    
+  b= a+1;    
+  int c;    
+  c= a+2;    
+  int d;    
+  d= a+3;    
+  int e;    
+  e= a+4;    
+  int f;    
+  f= a+5;    
+  cout<<b;    
+  cout<<c;    
+  cout<<d;    
+  cout<<e;    
+  cout<<f;
+}
+int main ()
+{    
+  cout<<" inserisci a: ";    
+   cin>>a;
+   cout<<" il valore di b: ";    
+   cout<<" il valore di c: ";    
+   cout<<" il valore di d: ";   
+   cout<<" il valore di e: ";    
+   cout<<" il valore di f: ";
+    successione (a) ;
+  
+  
+  
+  
+  
+  ES 38 p 128
+ 
+#include <iostream>
+    using namespace std;
+  float prezzo, perc_sconto, prezzo_scontato;
+  int sconto ( float prezzo,float perc_sconto, float prezzo_scontato)
+  {
+    prezzo_scontato= prezzo- ((prezzo*perc_perc_sconto)/100);
+    return prezzo_scontato; 
+  }
+  int main ()
+  {    
+    cout<<" inserire il Prezzo del Prodotto:  ";   
+    cin>>prezzo;   
+    cout<<" inserire lo sconto:  ";   
+    cin>>perc_sconto; 
+    cout<<endl;    
+    cout<<"il prezzo scontato è: "   
+    cout<<sconto (prezzo.perc_sconto.prezzo.scontato) <<endl ; 
+    return 0;}
