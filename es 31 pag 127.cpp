7a44ee20fd743e8638bc79886efe4040c13b912b
BigRussell0 <zetak24@gmail.com>
2016-11-06 21:58:36 +0100

    Update es 31 pag 127.cpp
    fatto!

--- es 31 pag 127.cpp
+++ es 31 pag 127.cpp
@@ -1,4 +1,2 @@
 // esercizio 31 pag 127
-#include <iostream>
-#include <string>
-    int valore (int n)
+#include <iostream>#include <string.h>using namespace std;string valore;void stampa (string valore){    cout<< valore << endl <<endl ;    cout<< valore << endl <<endl ;    cout<< valore << endl <<endl ;}int main (){    cout<<" dammi la stringa di nomi:   ";    cin>> valore;    cout<<endl;    stampa (valore) ;return 0;}
