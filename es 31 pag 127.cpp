0c6757ac0e6266cd388e4a30cc98614d0967af83
BigRussell0 <zetak24@gmail.com>
2016-11-06 22:26:16 +0100

    Update es 31 pag 127.cpp

--- es 31 pag 127.cpp
+++ es 31 pag 127.cpp
@@ -1,2 +1,18 @@
 // esercizio 31 pag 127
-#include <iostream>#include <string.h>using namespace std;string valore;void stampa (string valore){    cout<< valore << endl <<endl ;    cout<< valore << endl <<endl ;    cout<< valore << endl <<endl ;}int main (){    cout<<" dammi la stringa di nomi:   ";    cin>> valore;    cout<<endl;    stampa (valore) ;return 0;}
+#include <iostream>
+#include <string.h>
+using namespace std;
+string valore;
+void stampa (string valore)
+{    
+  cout<< valore << endl <<endl ;    
+  cout<< valore << endl <<endl ;    
+  cout<< valore << endl <<endl ;
+}
+int main ()
+{    
+  cout<<" dammi la stringa di nomi:   ";    
+  cin>> valore;    
+  cout<<endl;    
+  stampa (valore) ;
+  return 0;}
